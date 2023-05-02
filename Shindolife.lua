6f8842d4ad445d68809c6fbfe03a6598f383a06e
Pham Huy <102604673+HoangHuyPham@users.noreply.github.com>
2023-05-02 21:19:41 +0700

    Update Shindolife.lua

--- Shindolife.lua
+++ Shindolife.lua
@@ -1019,7 +1019,7 @@ UserInputService.InputBegan:Connect(
 )
 
 InitGUI(gamepasses)
-game.Players.LocalPlayer:FindFirstChild("statz", TIME_OUT).ChildAdded:Connect(function()
+game.Players.LocalPlayer:WaitForChild("statz", TIME_OUT).ChildAdded:Connect(function()
 	InitGUI(gamepasses)
 end)
 
