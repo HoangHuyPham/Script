2cc56be7812062a8037862b4df4bf703cb946a16
Pham Huy <102604673+HoangHuyPham@users.noreply.github.com>
2023-05-14 23:08:53 +0700

    Update MurderMysteryV2.lua

--- MurderMysteryV2.lua
+++ MurderMysteryV2.lua
@@ -500,8 +500,8 @@ end
 
 function Tool:Spectate()
 	while Tool.Gui.Frame2_SpectateBtn do
-		if Tool.Player.Camera.CameraSubject ~= Tool.Player.SpectatePlayer:FindFirstChild"Character":FindFirstChild"Humanoid" then
-			Tool.Player.Camera.CameraSubject = Tool.Player.SpectatePlayer:FindFirstChild"Character":FindFirstChild"Humanoid"
+		if Tool.Player.Camera.CameraSubject ~= Tool.Player.SpectatePlayer.CharacterAdded:Wait():FindFirstChild"Humanoid" then
+			Tool.Player.Camera.CameraSubject = Tool.Player.SpectatePlayer.CharacterAdded:Wait():FindFirstChild"Humanoid"
 		end
 		task.wait(1)
 	end
