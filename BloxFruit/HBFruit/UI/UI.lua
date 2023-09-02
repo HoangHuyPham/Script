6f1ab6cd4f82cbf4d3113117500c921a7102a0a1
Pham Huy <102604673+HoangHuyPham@users.noreply.github.com>
2023-09-02 17:47:37 +0700

    Add files via upload

---
+++ BloxFruit/HBFruit/UI/UI.lua
@@ -0,0 +1,188 @@
+repeat task.wait() until game:IsLoaded() and KRNL_LOADED and game.GameId == 994732206
+task.wait(3)
+
+-- Instances:
+
+local HBFruit = Instance.new("ScreenGui")
+local background = Instance.new("ImageLabel")
+local container = Instance.new("ScrollingFrame")
+local farmChest = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local ImageButton = Instance.new("ImageButton")
+local UIListLayout = Instance.new("UIListLayout")
+local UIListLayout_2 = Instance.new("UIListLayout")
+local stopAtBeli = Instance.new("Frame")
+local UIListLayout_3 = Instance.new("UIListLayout")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local fastMode = Instance.new("Frame")
+local TextLabel_3 = Instance.new("TextLabel")
+local ImageButton_2 = Instance.new("ImageButton")
+local UIListLayout_4 = Instance.new("UIListLayout")
+local TextLabel_4 = Instance.new("TextLabel")
+
+--Properties:
+
+HBFruit.Name = "HBFruit"
+HBFruit.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+HBFruit.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+background.Name = "background"
+background.Parent = HBFruit
+background.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+background.BorderColor3 = Color3.fromRGB(0, 0, 0)
+background.BorderSizePixel = 0
+background.Position = UDim2.new(0.654929578, 0, 0.204166666, 0)
+background.Size = UDim2.new(0, 320, 0, 480)
+background.Image = "rbxassetid://14646385618"
+
+container.Name = "container"
+container.Parent = background
+container.Active = true
+container.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+container.BackgroundTransparency = 0.500
+container.BorderColor3 = Color3.fromRGB(0, 0, 0)
+container.BorderSizePixel = 0
+container.Position = UDim2.new(0.0500000007, 0, 0.150000006, 0)
+container.Size = UDim2.new(0.899999976, 0, 0.800000012, 0)
+container.CanvasSize = UDim2.new(0, 0, 0, 0)
+container.HorizontalScrollBarInset = Enum.ScrollBarInset.ScrollBar
+container.ScrollBarThickness = 5
+container.VerticalScrollBarInset = Enum.ScrollBarInset.ScrollBar
+
+farmChest.Name = "farmChest"
+farmChest.Parent = container
+farmChest.BackgroundColor3 = Color3.fromRGB(155, 229, 128)
+farmChest.BorderColor3 = Color3.fromRGB(0, 0, 0)
+farmChest.BorderSizePixel = 0
+farmChest.NextSelectionDown = stopAtBeli
+farmChest.Size = UDim2.new(1, 0, 0.100000001, 0)
+
+TextLabel.Parent = farmChest
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.NextSelectionRight = ImageButton
+TextLabel.Size = UDim2.new(0.800000012, 0, 1, 0)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "Farm Chest"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 0)
+TextLabel.TextSize = 30.000
+TextLabel.TextWrapped = true
+
+ImageButton.Parent = farmChest
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.NextSelectionLeft = TextLabel
+ImageButton.Size = UDim2.new(0.200000003, 0, 1, 0)
+ImageButton.Image = "rbxassetid://14652140686"
+
+UIListLayout.Parent = farmChest
+UIListLayout.FillDirection = Enum.FillDirection.Horizontal
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+UIListLayout_2.Parent = container
+UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+
+stopAtBeli.Name = "stopAtBeli"
+stopAtBeli.Parent = container
+stopAtBeli.BackgroundColor3 = Color3.fromRGB(155, 229, 128)
+stopAtBeli.BorderColor3 = Color3.fromRGB(0, 0, 0)
+stopAtBeli.BorderSizePixel = 0
+stopAtBeli.Size = UDim2.new(1, 0, 0.100000001, 0)
+
+UIListLayout_3.Parent = stopAtBeli
+UIListLayout_3.FillDirection = Enum.FillDirection.Horizontal
+UIListLayout_3.SortOrder = Enum.SortOrder.LayoutOrder
+
+TextLabel_2.Parent = stopAtBeli
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.NextSelectionRight = ImageButton
+TextLabel_2.Size = UDim2.new(0.600000024, 0, 1, 0)
+TextLabel_2.Font = Enum.Font.SourceSansBold
+TextLabel_2.Text = "Stop at beli:"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 0)
+TextLabel_2.TextSize = 30.000
+TextLabel_2.TextWrapped = true
+
+TextBox.Parent = stopAtBeli
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BorderColor3 = Color3.fromRGB(255, 255, 127)
+TextBox.Size = UDim2.new(0.400000006, 0, 1, 0)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.PlaceholderText = "-1 is no limit"
+TextBox.Text = "-1"
+TextBox.TextColor3 = Color3.fromRGB(0, 255, 127)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+fastMode.Name = "fastMode"
+fastMode.Parent = container
+fastMode.BackgroundColor3 = Color3.fromRGB(155, 229, 128)
+fastMode.BorderColor3 = Color3.fromRGB(0, 0, 0)
+fastMode.BorderSizePixel = 0
+fastMode.NextSelectionUp = stopAtBeli
+fastMode.Size = UDim2.new(1, 0, 0.100000001, 0)
+
+TextLabel_3.Parent = fastMode
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.NextSelectionRight = ImageButton
+TextLabel_3.Size = UDim2.new(0.800000012, 0, 1, 0)
+TextLabel_3.Font = Enum.Font.SourceSansBold
+TextLabel_3.Text = "Fast Mode"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 0)
+TextLabel_3.TextSize = 30.000
+TextLabel_3.TextWrapped = true
+
+ImageButton_2.Parent = fastMode
+ImageButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton_2.BorderSizePixel = 0
+ImageButton_2.NextSelectionLeft = TextLabel_3
+ImageButton_2.Size = UDim2.new(0.200000003, 0, 1, 0)
+ImageButton_2.Image = "rbxassetid://14652140686"
+
+UIListLayout_4.Parent = fastMode
+UIListLayout_4.FillDirection = Enum.FillDirection.Horizontal
+UIListLayout_4.SortOrder = Enum.SortOrder.LayoutOrder
+
+TextLabel_4.Parent = background
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.NextSelectionRight = ImageButton
+TextLabel_4.Position = UDim2.new(0.0500000007, 0, 0.100000001, 0)
+TextLabel_4.Size = UDim2.new(0.200000003, 0, 0.0500000007, 0)
+TextLabel_4.Font = Enum.Font.SourceSansBold
+TextLabel_4.Text = "Misc"
+TextLabel_4.TextColor3 = Color3.fromRGB(9, 9, 9)
+TextLabel_4.TextSize = 25.000
+TextLabel_4.TextWrapped = true
+
+--Functions:
+local function toggle(instance)
+	if (instance:IsA("TextButton")) then
+		instance.Selected = not instance.Selected
+	elseif (instance:IsA("ImageButton")) then
+		instance.Selected = not instance.Selected
+		if (instance.Selected) then
+			instance.Image = "rbxassetid://14655571847"
+		else
+			instance.Image = "rbxassetid://14652140686"
+		end
+	end
+	if not (_G.HBFruit) then
+		repeat task.wait(1) until _G.HBFruit
+	end
+	_G.HBFruit.Function:saveAs()
+end
