c491f79bffd9361d4cde663e05bc31eb558c799f
Pham Huy <102604673+HoangHuyPham@users.noreply.github.com>
2023-09-04 22:46:20 +0700

    Update Main.lua

--- BF/HBFruit/Process/Main.lua
+++ BF/HBFruit/Process/Main.lua
@@ -11,10 +11,10 @@ local VirtualUser = game:GetService("VirtualUser")
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local SCRIPT_ID = "HBFruit"
 local UI = nil
-delay(delayTime, function()
+coroutine.resume(coroutine.create(function()
 	UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoangHuyPham/Script/testui/BF/HBFruit/UI/Main.lua", true))()
-end)
-task.wait(delayTime)
+end))
+task.wait(1)
 
 if JSON and TeleportService and VirtualUser and LocalPlayer and UI then
 	warn("Loading script successful!", os.clock()-initialTime.."s")
